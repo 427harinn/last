634c6669816e7d353d5fc83c1603c043d06df678
427harinn <rinnrenn424@yahoo.co.jp>
2021-12-18 22:33:34 +0900

    色々と試してみた

--- www/js/game.js
+++ www/js/game.js
@@ -1,12 +1,18 @@
 function startGame1() {
-  console.log("a");
+
+  active = true;
+  bactive = false;
+  tactive = false;
+  $("#btnA").prop("disabled", true);
+  startTime = new Date();
+  score = 0;
 }
 
 function startGame2() {
   console.log("b");
 }
 
-function startGame3(){
+function startGame3() {
   console.log("c");
 
 }
