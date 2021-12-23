b3b86d65e599d1a02c9c800b8ed5170e7ac0ddee
427harinn <rinnrenn424@yahoo.co.jp>
2021-12-23 09:31:37 +0900

    バックエンドに名前とスコアを送れたことを確認

--- www/js/game.js
+++ www/js/game.js
@@ -36,3 +36,36 @@ function startGame3() {
   startTime = new Date();
   score = 0;
 }
+
+function saveScore (name, score) {
+    // **********【問題１】名前とスコアを保存しよう！**********
+    
+var GameScore = ncmb.DataStore("GameScore_easy");
+var gameScore = new GameScore();
+
+gameScore.set("name", name);
+gameScore.set("score", score);
+
+gameScore.save()
+         .then(function (){
+             console.log("保存に成功しました。");
+         })
+         .catch(function (error){
+             console.log("保存に失敗しました。エラー:" + error); 
+         });
+}
+
+function imputName(score){
+    // 入力アラートを表示
+	var name = window.prompt("名前を入力してください", "");
+    if (name == null || name == "") {
+        $("#list-page p").html("保存がキャンセルされました");        
+    } else {
+        // スコアと入力した名前を保存
+        saveScore(name, score);
+        $("#list-page p").html(name + "さんのスコアは" + String(score) + "でした");
+    }
+    // ボタンの有効化
+    //document.gameForm.start.disabled = false;
+    //document.gameForm.ranking.disabled = false;
+}
